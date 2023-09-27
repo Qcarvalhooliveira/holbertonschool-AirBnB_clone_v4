6af396ef1cd8a442f00b7d79e8b6515d0d2104ca
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-09-27 11:21:16 +0200

    update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -36,12 +36,13 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
     cache_id = uuid.uuid4()
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
